17f296a9e00470f4398e67199b8040c295052977
Jameshak <76936549+Jameshak@users.noreply.github.com>
2021-08-10 12:45:52 +0630

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -48,7 +48,7 @@ x = setInterval(function() {
     hw = w / 2, // half-width
     hh = h / 2,
     opts = {
-      strings: ['HAPPY', 'Birthday!', ' ', config.name, ' ', 'Wish', 'You', 'all', 'the Best!V😇'],
+      strings: ['HAPPY', 'Birthday!', ' ', config.name, ' ', 'Wish', 'You', 'all', 'the Best! V'],
               
 
                
