9d2601a2f98250d33878a96ebe3e3b716fa316d2
Jameshak <76936549+Jameshak@users.noreply.github.com>
2022-02-18 15:26:10 +0630

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'March 9, 2021',
+  birthdate: 'March 9, 2022',
   name: '  Ms.DonnaMoon✨'
 };
 
